32d359b34d3a9fa32c2a331c033acafa71056354
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-10-18 21:45:53 -0400

    Create big-endian binary files

---
+++ test-programs/makeBinaryFile.rb
@@ -0,0 +1,9 @@
+# Write file of 65K 16-bit values to use as RAM values
+length = 2 ** 16
+# Default value
+a = Array.new(length) { 0x0407 }
+# Big endian
+s = a.pack("S>#{length}")
+File.open('program.exe', 'wb') do |f|
+  f.write s
+end
